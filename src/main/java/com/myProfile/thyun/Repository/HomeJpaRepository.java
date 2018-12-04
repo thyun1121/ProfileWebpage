9bb0079f107c3ec3972b222b8639992eaa1e7f68
david <thyun.ahn@gmail.com>
2018-12-04 11:29:45 +0900

    blot title css coding for mobile

--- src/main/java/com/myProfile/thyun/Repository/HomeJpaRepository.java
+++ src/main/java/com/myProfile/thyun/Repository/HomeJpaRepository.java
@@ -10,7 +10,6 @@ import com.myProfile.thyun.model.BlogPost;
 
 @EnableJpaRepositories("com.myProfile.thyun.Repository") 
 //https://jdm.kr/blog/121  참고.
-//https://matthew.kr/spring-boot-jpa%EC%97%90%EC%84%9C-update-%EC%BF%BC%EB%A6%AC-%EC%82%AC%EC%9A%A9%EC%8B%9C-xxx-is-not-mapped-%EC%97%90%EB%9F%AC-%EB%B0%9C%EC%83%9D%EC%8B%9C-%EB%8C%80%EC%B2%98%EB%B0%A9%EB%B2%95/ 참고.
 public interface HomeJpaRepository extends JpaRepository<BlogPost, String>{
 	@Query("SELECT  A.posts_no"
 			+",A.posts_title"
